67190077effaa6faabe9be8f8c12cf0137fb71b1
lowear <aaronrosslowe@gmail.com>
2022-11-01 23:57:43 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# newp 
+# newp - New Project
+This is a simple .bat file for initalising new python projects on my local machine.
+
+## Installation
+- Paste newp.bat into your default anaconda directory
+
+## Usage
+- Call the following command in anaconda prompt to initiate a new project
+
+```bash
+newp projectname
+```
---
+++ newp.bat
@@ -0,0 +1,41 @@
+:: This script uses the github cli api
+:: https://github.com/cli/cli
+:: Instuctions for use @ https://www.techielass.com/create-a-new-github-repository-from-the-command-line/
+:: gh auth login
+
+:: Set arguments
+set name=%1
+set root=C:\Users\aaron\git
+set envs=C:\Users\aaron\miniconda3\envs
+
+:: Create new conda env
+cmd /C conda create -n %name% python=3
+
+:: Create working directory
+mkdir %root%\%name%
+type nul > %root%\%name%\README.md
+echo # %name% >> %root%\%name%\README.md
+type nul > %root%\%name%\.gitignore
+echo .gitignore >> %root%\%name%\.gitignore
+echo github_repo.url >> %root%\%name%\.gitignore
+
+:: Create conda env Activate.d file
+mkdir %envs%\%name%\etc\conda\Activate.d
+type nul > %envs%\%name%\etc\conda\Activate.d\set_working_directory.bat
+echo @echo off >> %envs%\%name%\etc\conda\Activate.d\set_working_directory.bat
+echo cd %root%\%name% >> %envs%\%name%\etc\conda\Activate.d\set_working_directory.bat
+echo code . >> %envs%\%name%\etc\conda\Activate.d\set_working_directory.bat
+
+:: Init git repository
+cd %root%\%name%
+git init
+git add README.md
+git commit -m "first commit"
+gh repo create %name% --public --source=. --remote=upstream --push
+
+:: Create shortcut to git repository
+echo [InternetShortcut] > %root%\%name%\github_repo.url
+echo URL=https://github.com/lowear/%name% >> %root%\%name%\github_repo.url
+
+:: Activate the new environment
+activate %name%
